ea9719caa51e26ea8d0722442b80a59bf6c27c2c
eobuhov <eobuhov@accurateweb.ru>
2018-10-24 18:00:18 +0500

    [REZHANO-24] + заметки

---
+++ app/Doctrine/Migrations/Version20181024124602.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20181024124602 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cheese_stories CHANGE title title VARCHAR(256) DEFAULT NULL, CHANGE photo photo VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cheese_stories CHANGE title title VARCHAR(256) NOT NULL COLLATE utf8_general_ci, CHANGE photo photo VARCHAR(255) NOT NULL COLLATE utf8_general_ci');
+    }
+}
