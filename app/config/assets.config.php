2436ac8b5b245a273d1201abd6d920e7cc9ef278
gloginov <georgeloginov90@gmail.com>
2018-10-25 19:03:06 +0500

    [REZHANO-1] *новые макеты страница каталога *лайтбокс *меню

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,10 +3,10 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.3bdc54b2.css',
+                '/css/main.2e725ac8.css',
             ),
             'js' => array(
-                '/js/frontend.17606e01.js',
+                '/js/frontend.3ad86288.js',
             ),
         ),
     )
--- web/js/app/frontend/view/cart/add-to-cart-success-layer-view.js
+++ web/js/app/frontend/view/cart/add-to-cart-success-layer-view.js
@@ -1,42 +1,60 @@
 /**
  * Created by Денис on 06.06.2017.
  */
-define(function(require){
+define(function (require) {
   var ModalDialogView = require('view/dialog/base/modal-dialog-view');
 
   var template = _.template('\
         <div class="layer__close"></div>\
         <div class="layer__container">\
-          <div class="layer__title">Товар добавлен в корзину</div>\
-          <div class="add-to-cart-layer__wrap">\
-              <a href="<%= product_url %>" class="add-to-cart-layer__image" style="background: url(\'<%= preview_image %>\') center no-repeat">\
-              </a>\
-            <div class="add-to-cart-layer__info">\
-              <a href="<%= product_url %>" class="add-to-cart-layer__product-name"><%= name %></a>\
-              <div class="add-to-cart-layer__product-value"><%= quantity %> шт. &nbsp;&nbsp; <%= price.toCurrencyString() %></div>\
-            </div>\
+          <div class="layer__images">\
+            <a href="<%= product_url %>" class="show-full-image">\
+              <img src="<%= preview_image %>" alt="">\
+            </a>\
+            <a href="<%= product_url %>" class="show-full-image">\n              <img src="<%= preview_image %>" alt="">\n            </a>\
           </div>\
-        </div>\
-        <div class="buttons">\
-          <a class="button" href="<%= urlPrefix %>/cart">Перейти в корзину</a>\
-          <a class="button white continue-shopping" href="#">Продолжить покупки</a>\
-        </div>\
-<!--<div class="layer__close"></div>\-->\
-<!-- <div class="layer__in"> -->\
-      <!--<p><%= quantity %> x  <a href="<%= product_url %>"><%= name %></a> добавлен<% if (quantity > 1) {%>ы<% } %> в Вашу корзину.</p> -->\
-      <!-- <% if (quantityChanged) { %><% } %> -->\
-      <!--<h3>Добавление в корзину</h3>-->\
-      <!--<div class="order-wrap__item">-->\
-        <!--<img class="order-pic" src="<%= preview_image %>" alt="" >-->\
-        <!--<a href="#" class="order-name"><%= name %></a>-->\
-        <!--<span class="order-quantity"><%= quantity %> шт.</span>-->\
-        <!--<span class="order-price"><%= price.toCurrencyString() %></span>-->\
-      <!--</div>-->\
-      <!--<a class="button button-grey" href="#">Продолжить покупки</a>-->\
-      <!--<a class="button show__cart" href="<%= urlPrefix %>/cart">Показать корзину</a>-->\
-      <!--<br style="clear:both">-->\
-<!--</div>-->\
-');
+          <div class="layer__text">\
+            <h2 class="layer__title"><%= name %></h2>\
+            <div class="add-to-cart-layer__wrap">\
+              <div class="add-to-cart-layer__info">\
+                <div class="dd-to-cart-layer__info-type">\
+                  <span class="info-type-sign__title">Тип:</span>\
+                  <span class="info-type-sign__text"></span>\
+                </div>\
+                 <div class="dd-to-cart-layer__info-type">\
+                    <span class="info-type-sign__title">Выдержка:</span>\
+                    <span class="info-type-sign__text"></span>\
+                 </div>\
+                 <div class="dd-to-cart-layer__info-type">\
+                   <span class="info-type-sign__title">Вкус:</span>\
+                   <span class="info-type-sign__text"></span>\
+                 </div>\
+                  <div class="dd-to-cart-layer__info-type">\
+                    <span class="info-type-sign__title">С чем едят:</span>\
+                    <span class="info-type-sign__text"></span>\
+                  </div>\
+                <div class="add-to-cart-layer__product-value"><%= quantity %> шт. &nbsp;&nbsp; <%= price.toCurrencyString() %></div>\
+                    <div class="product-item__characteristics">\
+                      <span class="product-item__quantity">300 г  / </span>\
+                      <span class="product-item__price">1&nbsp;200&nbsp;₽</span>\
+                   </div>\
+                    <a href="" class="button">\
+                      <span>В корзину</span>\
+                    </a>\
+                   <div class="product-item__controls">\
+                      <span class="controls-title">количество</span>\
+                      <span class="controls-item controls-item__increase"></span>\
+                      <input type="text" class="custom-input" value="0">\
+                      <span class="controls-item controls-item__reduce"></span>\
+                   </div>\
+                    </div>\
+                    </div>\
+                  </div>\
+                   <div class="buttons">\
+                     <a class="button" href="<%= urlPrefix %>/cart">Перейти в корзину</a>\
+                     <a class="button white continue-shopping" href="#">Продолжить покупки</a>\n        </div>\
+                  </div>\
+        ');
 
   return ModalDialogView.extend({
     id: 'facebox',
@@ -48,43 +66,54 @@ define(function(require){
       'click .button-grey': 'onContinueClick',
       'click .showcart': 'onShowCartClick'
     },
-    initialize: function(options){
+    initialize: function (options) {
       this.quantity = options.quantity;
 
       ModalDialogView.prototype.initialize.apply(this, arguments);
     },
-    onCloseClick: function(e){
+    onCloseClick: function (e) {
       e.preventDefault();
 
       this.close();
     },
-    onContinueClick: function(e){
+    onContinueClick: function (e) {
       e.preventDefault();
 
       this.close();
     },
-    onShowCartClick: function(e){
+    onShowCartClick: function (e) {
 
     },
-    render: function(){
+    render: function () {
+      var _self = this;
+
       this.$el.html(template({
         'name': this.model.get('name'),
         'quantityChanged': this.model.previousAttributes().quantity != this.model.get('quantity'),
         'product_url': this.model.get('product').url,
         'quantity': this.quantity,
-        'price':this.model.get('price') * this.quantity,
+        'price': this.model.get('price') * this.quantity,
         'image': this.model.get('product').images instanceof Array && this.model.get('product').images[0] ? this.model.get('product').images[0] : '/images/medium-no_photo.png',
         'preview_image': this.model.get('product').preview_image ? this.model.get('product').preview_image : '/images/medium-no_photo.png'
       }));
 
+      $(function () {
+        _self.$('.layer__images').slick({
+          dots: false,
+          arrows: true,
+          infinite: true
+        });
+      });
+
+
       return this;
     },
-    close: function(){
+    close: function () {
       ModalDialogView.prototype.close.apply(this, arguments);
 
       this.dispose();
     },
-    show: function(){
+    show: function () {
       var self = this;
       this.$overlay.fadeIn();
       this.$el.fadeIn();
