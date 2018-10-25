fedf88a83b2f1052d2ca2f3ae9425c797cc9468d
gloginov <georgeloginov90@gmail.com>
2018-10-25 14:22:59 +0500

    [REZHANO-1] *каталог

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.cb3b2d1a.css',
+                '/css/main.3bdc54b2.css',
             ),
             'js' => array(
                 '/js/frontend.7a9d47d8.js',
