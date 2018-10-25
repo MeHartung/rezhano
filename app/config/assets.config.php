795bee73e2233d1d064976fcd8ec5025ef625eb0
gloginov <georgeloginov90@gmail.com>
2018-10-25 10:39:59 +0500

    [REZHANO-1] *фиксированное меню

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,10 +3,10 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.d4914db0.css',
+                '/css/main.c471e787.css',
             ),
             'js' => array(
-                '/js/frontend.f4696671.js',
+                '/js/frontend.60f2a25c.js',
             ),
         ),
     )
