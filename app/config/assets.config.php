1b7fc9188ea342b5061788d5a4242fe3560b62a9
gloginov <georgeloginov90@gmail.com>
2018-10-29 12:03:55 +0500

    [REZHANO-1] *страница формления товара

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.81588bba.css',
+                '/css/main.2332f15f.css',
             ),
             'js' => array(
                 '/js/frontend.78178c3d.js',
