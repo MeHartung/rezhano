6181fd534d3137c238a4e939c8f1ca2912ce6561
gloginov <georgeloginov90@gmail.com>
2018-10-29 20:14:25 +0500

    [REZHANO-1] *слайдер для главной

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.b3df3cb6.css',
+                '/css/main.afc0a458.css',
             ),
             'js' => array(
                 '/js/frontend.ab2f259b.js',
