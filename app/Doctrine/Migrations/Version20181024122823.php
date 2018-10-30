e977fdb29d3b203a3fe9351a81ec27a45f36f1b1
eobuhov <eobuhov@accurateweb.ru>
2018-10-30 18:02:45 +0500

    [REZHANO-7] * исправлена миграция

--- app/Doctrine/Migrations/Version20181024122823.php
+++ app/Doctrine/Migrations/Version20181024122823.php
@@ -16,6 +16,11 @@ class Version20181024122823 extends AbstractMigration
     $this->addSql('INSERT INTO `menu` (`name`, `head`, `foot`, `tree_left`, `tree_level`, `tree_right`) VALUES (\'Меню\', \'0\', \'0\', \'1\', \'0\', \'2\');');
   }
   
+  public function postUp(Schema $schema)
+  {
+    $this->addSql('UPDATE `menu` SET `parent_id`=LAST_INSERT_ID() WHERE `id`=LAST_INSERT_ID()');
+  }
+  
   public function down(Schema $schema)
   {
     // this down() migration is auto-generated, please modify it to your needs
