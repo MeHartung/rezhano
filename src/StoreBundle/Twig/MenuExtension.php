1247cd55f65b5dbf3c99fc4367cb6db552be1339
eobuhov <eobuhov@accurateweb.ru>
2018-10-30 14:25:39 +0500

    [REZHANO-7] * исправил меню

--- src/StoreBundle/Twig/MenuExtension.php
+++ src/StoreBundle/Twig/MenuExtension.php
@@ -13,13 +13,13 @@ use Symfony\Component\Routing\Router;
 class MenuExtension extends \Twig_Extension
 {
   private $request, $router;
-
+  
   function __construct(RequestStack $requestStack, Router $router)
   {
     $this->request = $requestStack->getCurrentRequest();
     $this->router = $router;
   }
-
+  
   public function getFilters()
   {
     return array(
@@ -27,19 +27,20 @@ class MenuExtension extends \Twig_Extension
       new \Twig_SimpleFilter('is_active', array($this, 'isActive')),
     );
   }
-
+  
   public function menuUrl($url)
   {
     if (strlen($url) && preg_match('/^\/([^\/]|$)/', $url))
     {
       $url = $this->request->getBaseUrl() . $url;
     }
-
+    
     return $url;
   }
   
   /**
    * Активен ли пункт меню
+   *
    * @param $menuUrl
    * @return bool
    */
@@ -50,7 +51,18 @@ class MenuExtension extends \Twig_Extension
     
     $currentRoutePrefix = substr($menuRoute, 0, strpos($menuRoute, '_'));
     
-    return $menuRoute == $currentRoute || strpos($currentRoute, $currentRoutePrefix) !== false ||
+    if (is_bool($currentRoutePrefix)) return false;
+    
+    if (strlen($currentRoutePrefix) > 0)
+    {
+      $menuRoute == $currentRoute || strpos($currentRoute, $currentRoutePrefix) !== false ||
       ($menuRoute == 'catalog_index' && $currentRoute == 'taxon');
+    }
+    else
+    {
+      return $menuRoute == $currentRoute;
+    }
+    
+    return false;
   }
 }
