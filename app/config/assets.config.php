fa8a7862d169300daf354f4ed6f9f5b66ad9f048
gloginov <georgeloginov90@gmail.com>
2018-10-31 16:52:47 +0500

    [REZHANO-1] *оптимизация под 2500 (мелкие фиксы по верстке)

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.dba4eaa8.css',
+                '/css/main.e1e6334a.css',
             ),
             'js' => array(
                 '/js/frontend.9d7d819d.js',
