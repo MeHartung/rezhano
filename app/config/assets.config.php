ccc981df9740d7d6f3bcb3f08b8a2f53509bb8f2
gloginov <georgeloginov90@gmail.com>
2018-11-01 19:20:07 +0500

    [REZHANO-1] *форма вопроса

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.b6159b4a.css',
+                '/css/main.3f9e7a1a.css',
             ),
             'js' => array(
                 '/js/frontend.445589cf.js',
