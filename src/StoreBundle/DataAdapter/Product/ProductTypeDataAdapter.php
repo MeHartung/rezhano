c380e3d6f290bc5dd6eabb141aa3e43a54a26482
eobuhov <eobuhov@accurateweb.ru>
2018-11-02 14:44:32 +0500

    [REZHANO-23] + тип товара в ObjectCahe

---
+++ src/StoreBundle/DataAdapter/Product/ProductTypeDataAdapter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace StoreBundle\DataAdapter\Product;
+
+use Accurateweb\ClientApplicationBundle\DataAdapter\ClientApplicationModelAdapterInterface;
+use Accurateweb\MediaBundle\Model\Media\Resource\MediaResource;
+use StoreBundle\Entity\Store\Catalog\Product\Attributes\Type\ProductType;
+
+class ProductTypeDataAdapter implements ClientApplicationModelAdapterInterface
+{
+  /**
+   * @param $subject ProductType
+   * @param array $options
+   * @return array
+   */
+  public function transform ($subject, $options = array())
+  {
+    /**
+     * @var $media MediaResource
+     */
+    return array(
+      'id' => $subject->getId(),
+      'name' => $subject->getName(),
+      'min_count' => $subject->getMinCount(),
+      'count_step' => $subject->getCountStep(),
+      'measured' => $subject->getMeasured(),
+    );
+  }
+
+  public function getModelName ()
+  {
+    return 'ProductType';
+  }
+
+  public function supports ($subject)
+  {
+    return $subject instanceof ProductType;
+  }
+}
