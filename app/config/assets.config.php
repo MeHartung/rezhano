9c80cf2a31d7add1d0987f1d42ec3f1ebef89926
agrinevich <agrinevich@accurateweb.ru>
2018-11-23 10:55:51 +0500

    [REZHANO-80] *тизер не наезжает на банер

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.20d1fbe4.css',
+                '/css/main.7a26f66f.css',
             ),
             'js' => array(
                 '/js/frontend.85e74d67.js',
