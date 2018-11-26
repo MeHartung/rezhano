89d506dee8c160cbe3731d6d10e69ed826cf2c35
gloginov <georgeloginov90@gmail.com>
2018-11-26 19:15:01 +0500

    [REZHANO-84] *адаптив главной страницы под разрешение +2560px

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.7a26f66f.css',
+                '/css/main.dff31b90.css',
             ),
             'js' => array(
                 '/js/frontend.9060fa75.js',
