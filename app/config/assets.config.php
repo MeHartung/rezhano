37284e586bd7f57d848f153a346403ee905aa5b7
gloginov <georgeloginov90@gmail.com>
2018-11-28 16:56:40 +0500

    [REZHANO-84] *сборка стилей z-index

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.316e9d56.css',
+                '/css/main.1619ef58.css',
             ),
             'js' => array(
                 '/js/frontend.7625a71e.js',
