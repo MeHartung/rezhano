2db15eb6ba3fa5fe5f4599b55f75a20039c487a9
agrinevich <agrinevich@accurateweb.ru>
2018-12-04 17:47:59 +0500

    [REZHANO-85]

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.1619ef58.css',
+                '/css/main.37c27260.css',
             ),
             'js' => array(
                 '/js/frontend.7625a71e.js',
