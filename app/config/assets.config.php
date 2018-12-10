835ab579fb88682d9a7f7d291e268f8a866968ed
gloginov <georgeloginov90@gmail.com>
2018-12-10 10:02:02 +0500

    [REZHANO-110] *медиа-запросы что бы парень прилипал к правому краю

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.46526881.css',
+                '/css/main.495647b5.css',
             ),
             'js' => array(
                 '/js/frontend.2f4a8b12.js',
