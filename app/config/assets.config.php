4a1cfbc0284ada0797db6554b96fab0eb931d2e3
gloginov <georgeloginov90@gmail.com>
2018-12-10 09:22:24 +0500

    [REZHANO-108] *высота кнопок, выравнивание текста в кнопках

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.8312aabc.css',
+                '/css/main.46526881.css',
             ),
             'js' => array(
                 '/js/frontend.2f4a8b12.js',
