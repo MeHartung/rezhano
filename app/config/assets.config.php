cd0e8c2ea407f4f86a3e1f9db9f4b0ba11a72ec8
gloginov <georgeloginov90@gmail.com>
2018-12-11 10:37:11 +0500

    [REZHANO-88] *верстка страница контактов

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.ebd4174c.css',
+                '/css/main.3bbc33a8.css',
             ),
             'js' => array(
                 '/js/frontend.d6c15166.js',
