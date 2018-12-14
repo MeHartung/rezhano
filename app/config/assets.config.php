69a225d8c20de5c54f991a040aca02c35b5b2460
gloginov <georgeloginov90@gmail.com>
2018-12-14 11:08:49 +0500

    [REZHANO] *изменение разметки страницы каталога ( просьба заказчика )

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.5a2ed2c4.css',
+                '/css/main.69782adb.css',
             ),
             'js' => array(
                 '/js/frontend.f8124fb1.js',
