cdb2b587d6666f6c5f65a570013936053b7b67ab
gloginov <georgeloginov90@gmail.com>
2019-01-15 15:50:38 +0500

    [REZHANO-124] *перебиты стандартные стили ui

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.b01897c6.css',
+                '/css/main.b7d849ce.css',
             ),
             'js' => array(
                 '/js/frontend.2a665e56.js',
