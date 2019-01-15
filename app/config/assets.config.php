c7b40a93fa79dce0622f9e7879141c3389228be0
gloginov <georgeloginov90@gmail.com>
2019-01-15 16:42:59 +0500

    [REZHANO-92] *добавлены -волны -картинка на слайд "Хорошая еда"

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.b7d849ce.css',
+                '/css/main.0f4d85a7.css',
             ),
             'js' => array(
                 '/js/frontend.2a665e56.js',
