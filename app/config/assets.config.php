10509d2df20e1ab99114ddcd58b154e803650be0
gloginov <georgeloginov90@gmail.com>
2019-01-15 15:44:38 +0500

    [REZHANO-123] *сдвиг сыра влево (до тени)

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.09cd97f2.css',
+                '/css/main.b01897c6.css',
             ),
             'js' => array(
                 '/js/frontend.2a665e56.js',
