9f0972a556caa5d52693dde65f57764a2aee0970
Denis N. Ragozin <dragozin@accurateweb.ru>
2019-01-18 13:41:03 +0500

    *Коммит для деплоя на прод

--- app/Doctrine/Migrations/Version20190118070053.php
+++ app/Doctrine/Migrations/Version20190118070053.php
@@ -15,7 +15,7 @@ class Version20190118070053 extends AbstractMigration
     // this up() migration is auto-generated, please modify it to your needs
     $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-    $this->addSql('ALTER TABLE moysklad_queue DROP FOREIGN KEY FK_A9D371F18D9F6D38');
+//    $this->addSql('ALTER TABLE moysklad_queue DROP FOREIGN KEY FK_A9D371F18D9F6D38');
     $this->addSql('ALTER TABLE moysklad_queue ADD CONSTRAINT FK_A9D371F18D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE');
     $this->addSql('ALTER TABLE notification_order DROP FOREIGN KEY FK_339D76618D9F6D38');
     $this->addSql('ALTER TABLE notification_order ADD CONSTRAINT FK_339D76618D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE');
