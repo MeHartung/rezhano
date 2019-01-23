b7892b11236b8988b5accb231675f5013c3c7e25
gloginov <georgeloginov90@gmail.com>
2019-01-23 14:49:32 +0500

    [REZHANO-139] *при наведении лого меняет плавно меняет цвет

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.f41baaba.css',
+                '/css/main.9a1a786e.css',
             ),
             'js' => array(
                 '/js/frontend.76713a7a.js',
