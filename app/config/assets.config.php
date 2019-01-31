12051cde669235552f9bdacda9edffc4c2169be0
gloginov <georgeloginov90@gmail.com>
2019-01-31 14:47:25 +0500

    [REZHANO-152] *клик зона о сырной карте

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.9a1a786e.css',
+                '/css/main.f481acab.css',
             ),
             'js' => array(
                 '/js/frontend.76713a7a.js',
