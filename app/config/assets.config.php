e54364c95f067c2d8c571b8da6519f410b15a3a6
gloginov <georgeloginov90@gmail.com>
2019-03-04 15:36:26 +0500

    [REZHANO-160] *адаптация для iphone

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.0c5f5beb.css',
+                '/css/main.da1358f4.css',
             ),
             'js' => array(
                 '/js/frontend.bfede8e0.js',
