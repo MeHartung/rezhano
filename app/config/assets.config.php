356ac34745c42668136b0c59c3a70b4ee41de37e
gloginov <georgeloginov90@gmail.com>
2019-03-05 15:41:21 +0500

    [REZHANO-160] *адаптация для iphone (ширина для горизонтального просмотра)

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.34224c8c.css',
+                '/css/main.83d984f4.css',
             ),
             'js' => array(
                 '/js/frontend.e8a95099.js',
