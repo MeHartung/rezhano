00fe13ec320240cec971a9566f79a2163cc45e07
gloginov <georgeloginov90@gmail.com>
2019-03-06 13:48:55 +0500

    [REZHANO-160] *о нас горизонтально

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.981e674c.css',
+                '/css/main.0623fec6.css',
             ),
             'js' => array(
                 '/js/frontend.f2a305d0.js',
