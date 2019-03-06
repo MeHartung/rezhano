40af3e81013c98aeba90bcecad2f1c7745c7b1db
gloginov <georgeloginov90@gmail.com>
2019-03-06 13:11:30 +0500

    [REZHANO-160] *футер на модилах вертикально

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.f937d2ba.css',
+                '/css/main.981e674c.css',
             ),
             'js' => array(
                 '/js/frontend.f2a305d0.js',
