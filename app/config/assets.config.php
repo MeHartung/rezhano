f59f933009051471ae54fd12abb4c8e7742e284b
gloginov <georgeloginov90@gmail.com>
2019-03-06 12:40:35 +0500

    [REZHANO-160] *страница о нас (портрет)

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.bf272cb2.css',
+                '/css/main.f937d2ba.css',
             ),
             'js' => array(
                 '/js/frontend.f2a305d0.js',
