4500d043baaf5dbe7fdc03d6431402236295c856
gloginov <georgeloginov90@gmail.com>
2019-03-07 12:36:02 +0500

    [REZHANO-160] *быстрый просмотр товара (горизонтально)

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.e851b073.css',
+                '/css/main.49a4a0db.css',
             ),
             'js' => array(
                 '/js/frontend.82b34ac6.js',
