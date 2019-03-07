5051880f5c451a0a904e07bbfb99a32852c65770
gloginov <georgeloginov90@gmail.com>
2019-03-07 19:30:17 +0500

    [REZHANO-160] *корзина

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.364edcb0.css',
+                '/css/main.f4d7ec21.css',
             ),
             'js' => array(
                 '/js/frontend.1749c407.js',
