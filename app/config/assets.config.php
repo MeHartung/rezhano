219bb791e47a39b2dc596eb409f83a5d9a1736c2
gloginov <georgeloginov90@gmail.com>
2019-03-09 13:01:08 +0500

    [REZHANO-160] *задать вопрос (кнопка и высота)

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.6cf2f7c9.css',
+                '/css/main.2d9142cc.css',
             ),
             'js' => array(
                 '/js/frontend.d5cedd35.js',
