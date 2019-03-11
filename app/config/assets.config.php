07f01a9a745fabc4c6653ba7c47a51ef4c8839d4
gloginov <georgeloginov90@gmail.com>
2019-03-11 15:15:49 +0500

    [REZHANO-132] *скролл в окне задать вопрос

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.0be4673f.css',
+                '/css/main.3c0e3af4.css',
             ),
             'js' => array(
                 '/js/frontend.bfaa8626.js',
