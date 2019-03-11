13be69c8f9fb2603e3f5051b6922c75abcf9597b
gloginov <georgeloginov90@gmail.com>
2019-03-11 14:49:24 +0500

    [REZHANO-141] *обычный текст в служебных сообщениях

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.655d5697.css',
+                '/css/main.723b7120.css',
             ),
             'js' => array(
                 '/js/frontend.261d661d.js',
