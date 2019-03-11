3e70fbab3ed1cf2aa460b2fbd7c72b0bf9e857d8
gloginov <georgeloginov90@gmail.com>
2019-03-11 15:25:06 +0500

    [REZHANO-144] *сделал все хотелки, одну картинку поднял, одну опустил

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.3c0e3af4.css',
+                '/css/main.84d26de1.css',
             ),
             'js' => array(
                 '/js/frontend.bfaa8626.js',
