7e78dba08e7e3f218d0d6a60db462eada1a698f9
gloginov <georgeloginov90@gmail.com>
2019-03-12 17:54:31 +0500

    [REZHANO-160] *страница экскурсии

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.4ad244b8.css',
+                '/css/main.488345c4.css',
             ),
             'js' => array(
                 '/js/frontend.6d1741e1.js',
