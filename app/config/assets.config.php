8e557975107cf50df23a698c8527102ccdcf39be
gloginov <georgeloginov90@gmail.com>
2019-03-12 18:52:23 +0500

    [REZHANO-160] *главная мелкие правки

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.688a6b31.css',
+                '/css/main.6c08a4ef.css',
             ),
             'js' => array(
                 '/js/frontend.6d1741e1.js',
