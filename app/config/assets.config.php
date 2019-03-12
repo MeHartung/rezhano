d5fbcc9d92c401a816b7f0370a060f0fa4c850b8
gloginov <georgeloginov90@gmail.com>
2019-03-12 18:07:37 +0500

    [REZHANO-160] *параметры почты

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.488345c4.css',
+                '/css/main.688a6b31.css',
             ),
             'js' => array(
                 '/js/frontend.6d1741e1.js',
