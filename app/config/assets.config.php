696e0e1bf10d1d88e95e03d15e35759f2751f931
gloginov <georgeloginov90@gmail.com>
2019-03-12 19:41:23 +0500

    [REZHANO-160] *фикс меню мелкие правки

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.937dfe42.css',
+                '/css/main.11d2a642.css',
             ),
             'js' => array(
                 '/js/frontend.7a5d96dc.js',
