18dcec685219a39a5bffa73be18f5aadf21bd491
gloginov <georgeloginov90@gmail.com>
2019-03-13 10:39:47 +0500

    [REZHANO-160] *ios (фикс меню)

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.28efd5e7.css',
+                '/css/main.591eaecb.css',
             ),
             'js' => array(
                 '/js/frontend.7a5d96dc.js',
