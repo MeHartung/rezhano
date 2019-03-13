e4cd75041f9dc6984173ca6ff94f7840fc82cc9e
gloginov <georgeloginov90@gmail.com>
2019-03-13 14:31:09 +0500

    [REZHANO-160] *фиксированные окна

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.b0a2ad21.css',
+                '/css/main.fd2dd27f.css',
             ),
             'js' => array(
                 '/js/frontend.38f40f4b.js',
