498f83595f7790e13fc48bc27632d3ef06194104
gloginov <georgeloginov90@gmail.com>
2019-03-13 15:36:03 +0500

    [REZHANO-160] *фикс меню

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.1777dcfa.css',
+                '/css/main.efff3ee4.css',
             ),
             'js' => array(
                 '/js/frontend.7eaa5568.js',
