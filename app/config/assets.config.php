27521258c341197102f7535df76e7d028734f73b
gloginov <georgeloginov90@gmail.com>
2019-03-13 09:56:34 +0500

    [REZHANO-160] *ios

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.ffe048a1.css',
+                '/css/main.ad0d90c6.css',
             ),
             'js' => array(
                 '/js/frontend.7a5d96dc.js',
