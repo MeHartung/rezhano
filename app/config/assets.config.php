f514a16d83d59374cdd6674ff101bb9bba92a53c
gloginov <georgeloginov90@gmail.com>
2019-03-13 14:47:58 +0500

    [REZHANO-160] *фиксированное развернутое меню

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.fd2dd27f.css',
+                '/css/main.a71698b9.css',
             ),
             'js' => array(
                 '/js/frontend.38f40f4b.js',
