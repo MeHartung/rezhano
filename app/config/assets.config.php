4f00268f219398b636c502a37c95350073e56ff1
gloginov <georgeloginov90@gmail.com>
2019-03-14 11:12:55 +0500

    [REZHANO-150] *отступы, ширина лого внизу.

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.36f0a9bd.css',
+                '/css/main.d6cc9dbe.css',
             ),
             'js' => array(
                 '/js/frontend.b3d3c2f9.js',
