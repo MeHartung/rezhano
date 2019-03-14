d8727587755612590ca13f0344e0643702d23d74
gloginov <georgeloginov90@gmail.com>
2019-03-14 19:48:38 +0500

    [REZHANO-167] *страница контакты

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.75041d62.css',
+                '/css/main.a4542c65.css',
             ),
             'js' => array(
                 '/js/frontend.7241edef.js',
