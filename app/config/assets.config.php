8b9c3a5d43d7e1d647adee3a7f0d99b50ad82463
gloginov <georgeloginov90@gmail.com>
2019-03-18 12:53:47 +0500

    [REZHANO-169] *на разрешении 2000px+ поправил текст у скидочной карты

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.6de56e6f.css',
+                '/css/main.70484109.css',
             ),
             'js' => array(
                 '/js/frontend.b1b8520e.js',
