8b7dd6742239ab9689d1feff7dc6fb4ec6c3d011
gloginov <georgeloginov90@gmail.com>
2019-03-18 09:02:11 +0500

    [REZHANO-140] *наведение на иконки на странице контакты

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.a4542c65.css',
+                '/css/main.6de56e6f.css',
             ),
             'js' => array(
                 '/js/frontend.7241edef.js',
