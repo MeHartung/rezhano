f058e1cce62569a21909e1d73156c365e85d5034
gloginov <georgeloginov90@gmail.com>
2019-03-18 18:06:53 +0500

    [REZHANO-167] *ios scrollTop

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.5fa58111.css',
+                '/css/main.2ee95ed8.css',
             ),
             'js' => array(
                 '/js/frontend.2d40aa22.js',
