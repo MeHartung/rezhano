09d88b2881c256ff739d590de59f9aa4227b913e
gloginov <georgeloginov90@gmail.com>
2019-03-18 13:12:47 +0500

    [REZHANO-167] *сырная заметка

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.70484109.css',
+                '/css/main.48d8d179.css',
             ),
             'js' => array(
                 '/js/frontend.b1b8520e.js',
