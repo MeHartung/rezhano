fbbdb6d448c65a78d083a914b3ce7726e9beb1a8
gloginov <georgeloginov90@gmail.com>
2019-03-19 15:17:04 +0500

    [REZHANO-167] *стили сборка

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.cef80b23.css',
+                '/css/main.0056c8c0.css',
             ),
             'js' => array(
                 '/js/frontend.447f3e0e.js',
