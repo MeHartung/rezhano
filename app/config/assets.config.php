6f59a28fac694ec4a35c54b83b2ce981a4ce14b5
gloginov <georgeloginov90@gmail.com>
2019-03-20 13:31:45 +0500

    [REZHANO-167] *текст оформления заказа

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.877f780a.css',
+                '/css/main.27e0bddf.css',
             ),
             'js' => array(
                 '/js/frontend.dedfdc29.js',
