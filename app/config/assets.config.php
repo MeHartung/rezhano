96fbcd0e6baf33980fcf3b66efb08f1d15911110
gloginov <georgeloginov90@gmail.com>
2019-03-20 13:01:41 +0500

    [REZHANO-167] *текст в корзине

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.4bb8a84f.css',
+                '/css/main.877f780a.css',
             ),
             'js' => array(
                 '/js/frontend.dedfdc29.js',
