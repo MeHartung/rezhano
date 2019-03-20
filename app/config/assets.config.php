a1e6703f175ced9fc4091310b0b4b973072f1ceb
gloginov <georgeloginov90@gmail.com>
2019-03-20 17:07:49 +0500

    [HOTFIX] *корзина

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.402ba514.css',
+                '/css/main.31b54d92.css',
             ),
             'js' => array(
                 '/js/frontend.27af5bc2.js',
