0f3d096e5d957f525fb2fcd7222ded3354007bed
gloginov <georgeloginov90@gmail.com>
2019-03-21 17:59:55 +0500

    [REZHANO-140] *кеширование иконок

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.5f92160e.css',
+                '/css/main.f38da8b3.css',
             ),
             'js' => array(
                 '/js/frontend.27af5bc2.js',
