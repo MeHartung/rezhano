7403bf58ac2a0028dde4e8ca86ed19ec08ed069e
gloginov <georgeloginov90@gmail.com>
2019-03-21 18:45:42 +0500

    [REZHANO-140] *фейд картинок для firefox

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.f38da8b3.css',
+                '/css/main.56ca1d95.css',
             ),
             'js' => array(
                 '/js/frontend.27af5bc2.js',
