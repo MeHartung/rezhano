2f13906a662250195f80f5b8ca0b5d93d8804b08
gloginov <georgeloginov90@gmail.com>
2019-04-04 15:36:34 +0500

    [REZHANO-176] *до загрузки картинки на главной фон черный

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.57a1526e.css',
+                '/css/main.8f188ae0.css',
             ),
             'js' => array(
                 '/js/frontend.27af5bc2.js',
