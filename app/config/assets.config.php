4e9464fae46ec968f13e1bab5c698a2844b73bc7
gloginov <georgeloginov90@gmail.com>
2019-04-05 16:04:56 +0500

    [REZHANO-182] *в разных макетах разные отступы снизу, приблизился уменьшил фоновую картинку

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.c162506c.css',
+                '/css/main.c82cb332.css',
             ),
             'js' => array(
                 '/js/frontend.901743d2.js',
