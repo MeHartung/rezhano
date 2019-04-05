5767650abd4b4f71b172b284efc07c84a5f10411
gloginov <georgeloginov90@gmail.com>
2019-04-05 11:59:50 +0500

    [REZHANO-187] *добавил отступы для партнеров

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.f9a4e394.css',
+                '/css/main.1b6f5f1d.css',
             ),
             'js' => array(
                 '/js/frontend.db9d6e06.js',
