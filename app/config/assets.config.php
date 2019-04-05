06c41f164b092a45e7fd8bdc8b5e1a940e490db8
gloginov <georgeloginov90@gmail.com>
2019-04-05 11:35:31 +0500

    [REZHANO-186] *отступ снизу у карточки товара

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.4d64206e.css',
+                '/css/main.f9a4e394.css',
             ),
             'js' => array(
                 '/js/frontend.db9d6e06.js',
