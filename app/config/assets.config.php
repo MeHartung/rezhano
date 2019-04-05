f7eceac4f54842cd631c35bb7d4eb574bd977b34
gloginov <georgeloginov90@gmail.com>
2019-04-05 12:51:05 +0500

    [REZHANO-189] *увеличил отступ у футера, опустил логотип

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.eed62e25.css',
+                '/css/main.da9f66de.css',
             ),
             'js' => array(
                 '/js/frontend.901743d2.js',
