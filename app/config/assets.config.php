89e6bd143b054caf0a54be9f889eeabc020074da
gloginov <georgeloginov90@gmail.com>
2019-04-05 13:04:04 +0500

    [REZHANO-192] *добавил svg иконку об ошибке формы

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.da9f66de.css',
+                '/css/main.dcdda077.css',
             ),
             'js' => array(
                 '/js/frontend.901743d2.js',
