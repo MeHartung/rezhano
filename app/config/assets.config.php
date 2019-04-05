0e9a12f4f614a4a2183c40a09b9112a6b58c3f2c
gloginov <georgeloginov90@gmail.com>
2019-04-05 13:16:37 +0500

    [REZHANO-193] *нужно оттестировать

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.dcdda077.css',
+                '/css/main.d5c64ade.css',
             ),
             'js' => array(
                 '/js/frontend.901743d2.js',
