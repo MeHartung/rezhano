9879b50ac63ab33b841d261b8a53686b84083f27
gloginov <georgeloginov90@gmail.com>
2019-04-05 17:33:39 +0500

    [REZHANO-183] *выравнивание товара в плитке товаров

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.eb77ccb7.css',
+                '/css/main.e0877138.css',
             ),
             'js' => array(
                 '/js/frontend.901743d2.js',
