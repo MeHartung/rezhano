a6ff59eca3d59898caf7fb43023e4d3e871bb7ec
gloginov <georgeloginov90@gmail.com>
2019-04-12 16:08:05 +0500

    [REZHANO-195]

--- app/config/assets.config.php
+++ app/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'app' => array(
             'css' => array(
-                '/css/main.833c8146.css',
+                '/css/main.52dfe46e.css',
             ),
             'js' => array(
                 '/js/frontend.1256fc01.js',
